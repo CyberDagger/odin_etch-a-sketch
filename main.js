a3922a8b1945c83689fa2674e66d824dc5e1f7cf
CyberDagger <cyberdagger003@gmail.com>
2025-04-09 14:04:43 +0100

    Draw 16x16 grid

---
+++ main.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container");
+
+const CONTAINER_WIDTH = 512;
+
+function drawGrid(squaresPerSide) {
+    let size = CONTAINER_WIDTH / squaresPerSide;
+    let numberCells = squaresPerSide ** 2;
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.height = size + "px";
+    cell.style.width = size + "px";
+    for (let i = 0; i < numberCells; i++) {
+        container.appendChild(cell.cloneNode(true));
+    }
+}
+
+drawGrid(16);
